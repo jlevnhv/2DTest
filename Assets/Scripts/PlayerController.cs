5c623db6dfb7e77b62e3743265ce1e434a4b801d
Josh Levinson <joshua.levinson@gmail.com>
2019-06-27 23:58:37 -0400

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        Vector2 curPos = transform.position;
+
+        Vector2 newPos = curPos + move * speed * Time.deltaTime;
+
+        transform.position = newPos;
+        
+    }
+}
